387e69f9fe177ef95a606df8869b5a94463d2e92
Indo Krish <55687559+Indok93@users.noreply.github.com>
2023-09-26 13:16:42 +0200

    assignments done

--- assets/js/opgaver/domelementer.js
+++ assets/js/opgaver/domelementer.js
@@ -8,16 +8,19 @@ Du skal finde elementet med id="yellowFigure" og console logge det.
 */
 
 // din kode her
-
+let figureRed = document.getElementById('redFigure');
+console.log(figureRed);
+let figureYellow = document.getElementById('yellowFigure');
+console.log(figureYellow);
 
 /* opgave 1.2
 Du skal ændre baggrundsfarve på de to elementer du har fundet i opgave 1.1
 farven på elementerne skal være blå. brug evt. elelement.style.backgroundColor
 */
 
-
 // din kode her
-
+figureRed.style.backgroundColor = 'blue';
+figureYellow.style.backgroundColor = 'blue';
 
 
 /* opgave 2.1
@@ -26,9 +29,16 @@ Du skal oprette følgende HTML elementer i Elementet med id opgaveTwo.
 - et p med teksten: Jeg har løst opgave 2.1
 */
 
-
 // din kode her
+let titleOne = document.createElement('h2');
+let paragraphOne = document.createElement('p');
 
+titleOne.innerText = 'Opgave 2.1 løsning';
+paragraphOne.innerText = 'Jeg har løst opgave 2.1';
+
+const HTMLdiv = document.getElementById('opgaveTwo');
+HTMLdiv.appendChild(titleOne);
+HTMLdiv.appendChild(paragraphOne);
 
 // opgave 3 liveHTML lists
 /* opgave 3.1
@@ -36,7 +46,8 @@ Du skal finde alle elementer med klassen purpleFigures og console logge resultat
 */
 
 // din kode her
-
+let figurePurple = document.getElementsByClassName('purpleFigures');
+console.log(figurePurple);
 
 /* opgave 3.2
 Du skal finde alle elementer med klassen purpleFigures og bruge array.from() til at konvertere listen
@@ -45,6 +56,10 @@ array.from beskrivelse HER: https://developer.mozilla.org/en-US/docs/Web/JavaScr
 */
 
 // din kode her
+let PurpleFigures = Array.from(figurePurple);
+PurpleFigures.map((figure) => {
+    figure.style.backgroundColor = 'red';
+})
 
 
 /* opgave 3.3
@@ -52,8 +67,10 @@ Brug din array til at ændre h3 elementets, inden i purpleFigures, tekst til RED
 myElement.children[0].innerHTML til at ændre 1. child elements inner html.
 */
 
-
 // din kode her
+PurpleFigures.forEach((figure) => {
+    figure.children[0].innerHTML = `<h3>RED</h3>`;
+});
 
 
 
@@ -71,7 +88,20 @@ const myData = {
 
 
 // din kode her
+let heading = document.createElement('h4');
+let img = document.createElement('img');
+let description = document.createElement('p');
 
+heading.innerHTML = myData.name;
+img.src = myData.image;
+description.innerHTML = myData.description;
 
+img.setAttribute('style', 'width: 100%');
+img.setAttribute('alt', 'image-of-tiger');
 
+const myDomElement = document.getElementById('opgaveFour')
 
+myDomElement.appendChild(heading);
+myDomElement.appendChild(img);
+myDomElement.appendChild(description);
+console.log(myDomElement);
